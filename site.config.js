2473166b43b669f63c061c838bd300abaf7bc090
dsrogerssojo <david.rogers@sojoindustries.com>
2026-06-12 09:58:16 -0400

    Add location configuration

---
+++ site.config.js
@@ -0,0 +1,15 @@
+window.SOJO_LOOKUP_CONFIG = {
+  APP_TITLE: "Sojo Chemical Product Lookup",
+  COMPANY_NAME: "Sojo Industries",
+  LOCATION_NAME: "Langhorne - PA",
+  LOCATION_SLUG: "langhorne-pa",
+  EXCEL_SOURCES: [
+    "data/current-sds-list.xlsx",
+    "https://raw.githubusercontent.com/dsrogerssojo/SojoChemicalProductLookup/main/data/current-sds-list.xlsx"
+  ],
+  CSV_FALLBACK_PATH: "data/langhorne-sds.csv",
+  SDS_SHEET_NAME: "SDS Info",
+  INVENTORY_SHEET_NAME: "Inventory",
+  EMERGENCY_PHONE: "911",
+  POISON_CONTROL_PHONE: "1-800-222-1222"
+};
