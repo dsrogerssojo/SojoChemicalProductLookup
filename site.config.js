eb54a01637dff4d5028563cd09fb640bd920ea3e
dsrogerssojo <david.rogers@sojoindustries.com>
2026-06-12 10:42:55 -0400

    Switch site config to same-origin CSV source

--- site.config.js
+++ site.config.js
@@ -3,13 +3,7 @@ window.SOJO_LOOKUP_CONFIG = {
   COMPANY_NAME: "Sojo Industries",
   LOCATION_NAME: "Langhorne - PA",
   LOCATION_SLUG: "langhorne-pa",
-  EXCEL_SOURCES: [
-    "https://drive.google.com/uc?export=download&id=14RE_jqQ3FF-gGSRRlptxJ3rbM3AIK_bI",
-    "https://docs.google.com/file/d/14RE_jqQ3FF-gGSRRlptxJ3rbM3AIK_bI/edit?usp=docslist_api&filetype=msexcel"
-  ],
-  CSV_FALLBACK_PATH: "",
-  SDS_SHEET_NAME: "SDS Info",
-  INVENTORY_SHEET_NAME: "Inventory",
+  CSV_SOURCE_PATH: "data/sds.csv",
   EMERGENCY_PHONE: "911",
   POISON_CONTROL_PHONE: "1-800-222-1222"
 };
