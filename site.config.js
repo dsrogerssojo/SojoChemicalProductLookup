09ee34bcadcfd801ce3d593805dd437bdb675020
dsrogerssojo <david.rogers@sojoindustries.com>
2026-06-22 11:06:56 -0400

    Use signed Power Automate request URL

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 window.SOJO_LOOKUP_CONFIG = {
   APP_TITLE: "Sojo SDS Lookup",
   COMPANY_NAME: "Sojo Industries",
-  NEW_CHEMICAL_REQUEST_URL: "https://defaultd5742e257fbf45b09478ae0f1d1eb4.4e.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/f699767ef2b34de5990b05f8edcb53db/triggers/manual/paths/invoke?api-version=1",
+  NEW_CHEMICAL_REQUEST_URL: "https://defaultd5742e257fbf45b09478ae0f1d1eb4.4e.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/f699767ef2b34de5990b05f8edcb53db/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=E-ORYQWJwIpLLdovbyakEl08GyyJV6_HofbDCqGVazY",
   LOCATIONS: [
     {
       name: "Langhorne - PA",
